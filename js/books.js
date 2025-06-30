6a31b5c19876c0369b8832328ce30d8c0b9e70f9
bayubanty <98555489+bayubanty@users.noreply.github.com>
2025-06-30 19:51:27 -0400

    Update books.js

--- js/books.js
+++ js/books.js
@@ -1,6 +1,5 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const books = [
-   {
+const books = [
+  {
     title: "HTML and CSS: Design and Build Websites",
     authors: ["Jon Duckett"],
     publisher: "Wiley",
@@ -8,15 +7,15 @@ document.addEventListener("DOMContentLoaded", () => {
     thumbnail: "https://books.google.com/books/content?id=aGjaBTbT0o0C&printsec=frontcover&img=1&zoom=1",
     link: "https://books.google.com/books/about/HTML_and_CSS.html?id=aGjaBTbT0o0C"
   },
-    {
-      title: "Eloquent JavaScript",
-      authors: ["Marijn Haverbeke"],
-      publisher: "No Starch Press",
-      publishedDate: "2018",
-      thumbnail: "https://eloquentjavascript.net/img/cover.jpg",
-      link: "https://eloquentjavascript.net/"
-    },
-    {
+  {
+    title: "Eloquent JavaScript",
+    authors: ["Marijn Haverbeke"],
+    publisher: "No Starch Press",
+    publishedDate: "2024",
+    thumbnail: "https://eloquentjavascript.net/img/cover.jpg",
+    link: "https://eloquentjavascript.net/"
+  },
+  {
     title: "You Don’t Know JS",
     authors: ["Kyle Simpson"],
     publisher: "O'Reilly Media",
@@ -24,7 +23,7 @@ document.addEventListener("DOMContentLoaded", () => {
     thumbnail: "https://images-na.ssl-images-amazon.com/images/I/81kqrwS1nNL.jpg",
     link: "https://github.com/getify/You-Dont-Know-JS"
   },
-    {
+  {
     title: "The HTML and CSS Workshop",
     authors: ["Lewis Coulson", "Brett Jephson", "Rob Larsen", "Matt Park", "Marian Zburlea"],
     publisher: "Packt Publishing Ltd",
@@ -32,24 +31,24 @@ document.addEventListener("DOMContentLoaded", () => {
     thumbnail: "https://books.google.com/books/content?id=gVjBDwAAQBAJ&printsec=frontcover&img=1&zoom=1",
     link: "https://play.google.com/store/books/details/The_HTML_and_CSS_Workshop?id=gVjBDwAAQBAJ"
   }
-  ];
-  const bookListDiv = document.getElementById("book-list");
+];
 
+const container = document.getElementById("book-list");
+
+if (container) {
   books.forEach(book => {
-    const bookCard = document.createElement("div");
-    bookCard.classList.add("book-card");
-
-    bookCard.innerHTML = `
-      <img src="${book.image}" alt="${book.title} Cover">
+    const div = document.createElement("div");
+    div.className = "book-card";
+    div.innerHTML = `
+      <img src="${book.thumbnail}" alt="Cover of ${book.title}">
       <div class="book-info">
         <h3>${book.title}</h3>
-        <p><strong>Author:</strong> ${book.author}</p>
+        <p><strong>Author(s):</strong> ${book.authors.join(", ")}</p>
         <p><strong>Publisher:</strong> ${book.publisher}</p>
-        <p><strong>Published:</strong> ${book.year}</p>
-        <p><a href="${book.link}" target="_blank">View on Google Books</a></p>
+        <p><strong>Published:</strong> ${book.publishedDate}</p>
+        <p><a href="${book.link}" target="_blank">More Info</a></p>
       </div>
     `;
-
-    bookListDiv.appendChild(bookCard);
+    container.appendChild(div); // Append each book's card
   });
-});
+}
