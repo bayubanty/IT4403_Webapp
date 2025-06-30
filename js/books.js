a6f4ffc7b8a37c0e616ac8f737fa148855a8b53d
bayubanty <98555489+bayubanty@users.noreply.github.com>
2025-06-30 19:08:20 -0400

    Update books.js

--- js/books.js
+++ js/books.js
@@ -1,5 +1,6 @@
-const books = [
-  {
+document.addEventListener("DOMContentLoaded", () => {
+  const books = [
+   {
     title: "HTML and CSS: Design and Build Websites",
     authors: ["Jon Duckett"],
     publisher: "Wiley",
@@ -7,15 +8,15 @@ const books = [
     thumbnail: "https://books.google.com/books/content?id=aGjaBTbT0o0C&printsec=frontcover&img=1&zoom=1",
     link: "https://books.google.com/books/about/HTML_and_CSS.html?id=aGjaBTbT0o0C"
   },
-  {
-    title: "Eloquent JavaScript",
-    authors: ["Marijn Haverbeke"],
-    publisher: "No Starch Press",
-    publishedDate: "2024",
-    thumbnail: "https://eloquentjavascript.net/img/cover.jpg",
-    link: "https://eloquentjavascript.net/"
-  },
-  {
+    {
+      title: "Eloquent JavaScript",
+      authors: ["Marijn Haverbeke"],
+      publisher: "No Starch Press",
+      publishedDate: "2018",
+      thumbnail: "https://eloquentjavascript.net/img/cover.jpg",
+      link: "https://eloquentjavascript.net/"
+    },
+    {
     title: "You Don’t Know JS",
     authors: ["Kyle Simpson"],
     publisher: "O'Reilly Media",
@@ -23,7 +24,7 @@ const books = [
     thumbnail: "https://images-na.ssl-images-amazon.com/images/I/81kqrwS1nNL.jpg",
     link: "https://github.com/getify/You-Dont-Know-JS"
   },
-  {
+    {
     title: "The HTML and CSS Workshop",
     authors: ["Lewis Coulson", "Brett Jephson", "Rob Larsen", "Matt Park", "Marian Zburlea"],
     publisher: "Packt Publishing Ltd",
@@ -31,9 +32,14 @@ const books = [
     thumbnail: "https://books.google.com/books/content?id=gVjBDwAAQBAJ&printsec=frontcover&img=1&zoom=1",
     link: "https://play.google.com/store/books/details/The_HTML_and_CSS_Workshop?id=gVjBDwAAQBAJ"
   }
-];
+  ];
 
- const container = document.getElementById("book-list");
+  const container = document.getElementById("book-list");
+
+  if (!container) {
+    console.error("Could not find #book-list");
+    return;
+  }
 
   books.forEach(book => {
     const div = document.createElement("div");
