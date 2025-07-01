b22b701a0e238a837f9ba31d7d915c97b78314da
bayubanty <98555489+bayubanty@users.noreply.github.com>
2025-06-30 22:28:40 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
+// script.js
+const container = document.getElementById("book-list");
+
+books.forEach(book => {
+  const div = document.createElement("div");
+  div.className = "book-card";
+  div.innerHTML = `
+    <img src="${book.thumbnail}" alt="Cover of ${book.title}">
+    <div class="book-info">
+      <h3>${book.title}</h3>
+      <p><strong>Author(s):</strong> ${book.authors.join(", ")}</p>
+      <p><strong>Publisher:</strong> ${book.publisher}</p>
+      <p><strong>Published:</strong> ${book.publishedDate}</p>
+      <p><a href="${book.link}" target="_blank">More Info</a></p>
+    </div>
+  `;
+  container.appendChild(div);
+});
 
